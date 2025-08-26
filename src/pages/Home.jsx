4983dbe7008341a53f0e82e71d5582efd15f19e2
seeyannewaz <shabbarseeyan@gmail.com>
2025-08-26 10:31:27 -0500

    Cleaned code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,6 @@ import { ThemeToggle } from "../components/ThemeToggle";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Theme Toggle */}
-      
-
       {/* Background Effects */}
       <StarBackground />
 
