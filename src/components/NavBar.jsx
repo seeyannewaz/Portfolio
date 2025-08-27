cb2d0faecc0b805239db001567eead3340ef8a85
seeyannewaz <shabbarseeyan@gmail.com>
2025-08-27 16:50:14 -0500

    Added Education section to Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ import { ThemeToggle } from "./ThemeToggle";
 const navItems = [
   { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
+  { name: "Education", href: "#education" },
   { name: "Skills", href: "#skills" },
   { name: "Experience", href: "#experience" },
   { name: "Projects", href: "#projects" },
